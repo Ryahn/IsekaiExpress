5bce80a135555dddaf52a543cb78fd05770e3e47
Ryahn <ryancarr10@gmail.com>
2024-09-26 09:01:20 +0000

    small update

--- bot/commands/slashCommands/funny_cage.js
+++ bot/commands/slashCommands/funny_cage.js
@@ -13,11 +13,7 @@ module.exports = {
     async execute(client, interaction) {
         let targetUser = interaction.options.getUser('target');
 
-			let messageContent = `<@${targetUser.id}>
-			Hello Caged user. You're detained under Paragraph 6 of Schedule 7 to the Terrorism Act 2000. You will not be detained for over 96 hours. You have the right and duty to remain silent.
-
-As always your safety is our priority,
--The Staff Team`;
+			let messageContent = `<@${targetUser.id}>\nHello Caged user. You're detained under Paragraph 6 of Schedule 7 to the Terrorism Act 2000. You will not be detained for over 96 hours. You have the right and duty to remain silent.\n\nAs always your safety is our priority,\n-The Staff Team`;
 		
 
         await interaction.reply(messageContent);
