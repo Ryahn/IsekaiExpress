3670dfb4864bac144d7c4c2fcf93eec9c90676f5
Ryahn <ryancarr10@gmail.com>
2024-10-03 21:18:56 +0000

    added femboy command

---
+++ bot/commands/slashCommands/femboy.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { getRandomColor } = require('../../utils/functions');
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+module.exports = {
+    enable: true,
+    data: new SlashCommandBuilder()
+        .setName('femboy')
+        .setDescription("femboy")
+		.addStringOption(option => option.setName('query').setDescription('The query to search for').setRequired(true)),
+
+    async execute(client, interaction) {
+    
+	const query = interaction.options.getString('query');
+
+	const api = `https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=100&tags=${query}&api_key=${process.env.API_KEY}&user_id=${process.env.USER_ID}`;
+
+	const response = await fetch(api);
+	if (!response.ok) return interaction.reply({ content: 'No results found', ephemeral: true });
+
+	const data = await response.json();
+	if (!data.post || data.post.length <= 0) return interaction.reply({ content: 'No results found', ephemeral: true });
+
+	const index = Math.floor(Math.random() * data.post.length);
+	
+	const post = data.post[index];
+	const sourceUrl = post.source.startsWith('http://') || post.source.startsWith('https://') 
+	? post.source 
+	: `https://${post.source}`;
+
+	const embed = new MessageEmbed()
+		.setTitle('Femboy')
+		.setDescription(`${interaction.user} wants ${query}`)
+		.addFields({ name: 'Tags', value: `${post.tags}`, inline: false })
+		.addFields({ name: 'Source', value: `[Click here](${sourceUrl})`, inline: false })
+		.setColor(`#${getRandomColor()}`)
+
+	await interaction.reply({ embeds: [embed] });
+	await interaction.followUp(`|| ${post.file_url} ||`);
+
+    },
+};
