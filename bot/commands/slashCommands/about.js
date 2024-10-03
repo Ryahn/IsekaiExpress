0cd07f256795e84c4bf4d7c95c2f50bbea2d8419
Ryahn <ryancarr10@gmail.com>
2024-10-03 21:39:00 +0000

    Added about slash command

---
+++ bot/commands/slashCommands/about.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { getRandomColor } = require('../../utils/functions');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    enable: true,
+    data: new SlashCommandBuilder()
+        .setName('about')
+        .setDescription("about this bot"),
+
+    async execute(client, interaction) {
+
+        const embed = new MessageEmbed()
+            .setTitle('About this bot')
+            .setColor(`#${getRandomColor()}`)
+            .addFields([{ name: 'Bot Name', value: 'IsekaiExpress', inline: true },
+				{ name: 'Bot Version', value: '1.1.5', inline: true },
+				{ name: 'Bot Author', value: '<@72884988374167552>', inline: true },
+				{ name: 'Bot Support', value: 'Contact <@72884988374167552>', inline: true }
+			])
+			.setFooter({ text: 'IsekaiExpress', iconURL: client.user.displayAvatarURL() })
+			.setTimestamp();
+
+        await interaction.reply({ embeds: [embed] });
+
+    },
+};
